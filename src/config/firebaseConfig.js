14be2e9fd146ff7b2e4110a4c3ca28e51f94da97
Chaoo Charles <charleykairu@gmail.com>
2020-05-29 12:43:39 +0300

    Adding react-redux-firebase v3

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB5nMtLlWC9TaNamN3SWO-1sozXbeih2do",
+  authDomain: "todo-d1b7e.firebaseapp.com",
+  databaseURL: "https://todo-d1b7e.firebaseio.com",
+  projectId: "todo-d1b7e",
+  storageBucket: "todo-d1b7e.appspot.com",
+  messagingSenderId: "456019758360",
+  appId: "1:456019758360:web:928bbecc2dab4b313d226d",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebaseConfig.firestore();
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./Reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { getFirebase, ReactReduxFireabseProvider } from "react-redux-firebase";
+import firebase from "./config/firebaseConfig";
+import { createFirestoreInstace } from "redux-firestore";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk.withExtraArgument({ getFirebase }))
+);
+
+const rrfProps = {
+  firebase,
+  config: {},
+  dispatch: store.dispatch,
+  createFirestoreInstace,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFireabseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFireabseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
