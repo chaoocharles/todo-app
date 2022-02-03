46aa7dd7f53a760caa8754facb078f34a0d2949d
Daniel Rivers <me@danielrivers.com>
2022-02-03 08:30:11 +0000

    [fix] make the project work

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,10 +3,10 @@ import "firebase/firestore";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB5nMtLlWC9TaNamN3SWO-1sozXbeih2do",
-  authDomain: "todo-d1b7e.firebaseapp.com",
-  databaseURL: "https://todo-d1b7e.firebaseio.com",
-  projectId: "todo-d1b7e",
+  apiKey: "AIzaSyA7fxogBZE1BBy1uASm2HsNPaF5vxMyRQo",
+  authDomain: "todo-list-e89a1.firebaseapp.com",
+  databaseURL: "https://todo-list-e89a1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todo-list-e89a1",
   storageBucket: "todo-d1b7e.appspot.com",
   messagingSenderId: "456019758360",
   appId: "1:456019758360:web:928bbecc2dab4b313d226d",
